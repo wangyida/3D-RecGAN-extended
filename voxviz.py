00661316cfe4f45ced2b2e40cdc4fcf45a60f377
Yida Wang <yidawang.cn@gmail.com>
2018-05-19 11:27:16 +0200

    images

--- voxviz.py
+++ voxviz.py
@@ -1,8 +1,45 @@
 import matplotlib.pyplot as plt
+import os
 import numpy as np
 from matplotlib import cm
 from skimage.transform import resize
 import argparse
+
+
+class ScanFile(object):
+    def __init__(self, directory, prefix=None, postfix='.jpg'):
+        self.directory = directory
+        self.prefix = prefix
+        self.postfix = postfix
+
+    def scan_files(self):
+        files_list = []
+
+        for dirpath, dirnames, filenames in os.walk(self.directory):
+            '''''
+            dirpath is a string, the path to the directory.
+            dirnames is a list of the names of the subdirectories in dirpath
+            (excluding '.' and '..').
+            filenames is a list of the names of the non-directory files
+            in dirpath.
+            '''
+            for special_file in filenames:
+                if self.postfix:
+                    special_file.endswith(self.postfix)
+                    files_list.append(os.path.join(dirpath, special_file))
+                elif self.prefix:
+                    special_file.startswith(self.prefix)
+                    files_list.append(os.path.join(dirpath, special_file))
+                else:
+                    files_list.append(os.path.join(dirpath, special_file))
+
+        return files_list
+
+    def scan_subdir(self):
+        subdir_list = []
+        for dirpath, dirnames, files in os.walk(self.directory):
+            subdir_list.append(dirpath)
+        return subdir_list
 
 
 def normalize(arr):
@@ -19,10 +56,12 @@ def show_histogram(values):
 
     plt.show()
 
+
 def explode(data):
     shape_arr = np.array(data.shape)
     size = shape_arr[:3]*2 - 1
-    exploded = np.zeros(np.concatenate([size, shape_arr[3:]]), dtype=data.dtype)
+    exploded = np.zeros(
+            np.concatenate([size, shape_arr[3:]]), dtype=data.dtype)
     exploded[::2, ::2, ::2] = data
     return exploded
 
@@ -40,47 +79,74 @@ def scale_by(arr, fac):
     return (arr-mean)*fac + mean
 
 
-def plot_cube(cube, angle=320):
+def plot_cube(cube, name='voxel.png', angle=140, IMG_DIM=50):
     from mpl_toolkits.mplot3d import Axes3D
     cube = normalize(cube)
 
-    facecolors = cm.hsv(cube)
-    facecolors[:, :, :, -1] = cube
+    facecolors = cm.gist_rainbow(cube)
+    # make the alpha channel more similar to each others while 0 is still 0
+    facecolors[:, :, :, -1] = 0.2 * np.tanh(cube * 1000)
     facecolors = explode(facecolors)
     filled = facecolors[:, :, :, -1] != 0
 
     x, y, z = expand_coordinates(np.indices(np.array(filled.shape) + 1))
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.view_init(30, angle)
-    ax.set_xlim(right=IMG_DIM*2)
-    ax.set_ylim(top=IMG_DIM*2)
-    ax.set_zlim(top=IMG_DIM*2)
-    ax.set_axis_off()
-    ax.set_aspect('equal')
+    fig = plt.figure(figsize=(30/2.54, 30/2.54))
+    # plot
+    ax1 = fig.add_subplot(111, projection='3d')
+    ax1.view_init(20, angle)
+    ax1.set_xlim(right=IMG_DIM*2)
+    ax1.set_ylim(top=IMG_DIM*2)
+    ax1.set_zlim(top=IMG_DIM*2)
+    ax1.set_axis_off()
+    ax1.voxels(x, y, z, filled, facecolors=facecolors)
 
-    ax.voxels(x, y, z, filled, facecolors=facecolors)
     # plt.show()
-    plt.savefig('voxel.png', transparent=True)
+    plt.savefig(name, bbox_inches='tight', pad_inches=0, transparent=True)
     plt.close(fig)
 
 
-def plot_image(arr):
+def plot_image(arr, name='depth.png'):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     # ax.set_axis_off()
     arr = (arr-np.min(arr))/(np.max(arr) - np.min(arr)) * 255
     arr = np.uint8(arr)
     ax.set_axis_off()
-    ax.set_aspect('equal')
+    # ax.set_aspect('equal')
 
     plt.imshow(arr, cmap="hot")
-    plt.savefig('depth.png', transparent=True)
+    plt.savefig(name, bbox_inches='tight', pad_inches=0, transparent=True)
     plt.close(fig)
 
 
-if __name__=="__main__":
+def plot_depvox(dir_dep, dir_vox, target_folder):
+    label_start = dir_dep.rfind('/')+1
+    label_end = dir_dep.find('.', label_start)
+    arr = np.load(dir_dep)
+    plot_image(
+            arr,
+            name=target_folder+'/depth/'+dir_dep[label_start:label_end]+'.png')
+
+    arr = np.load(dir_vox)
+
+    # ignore 255 and replace it with 0
+    arr[arr == 255] = 0
+
+    # show_histogram(arr)
+    """
+    transformed = np.clip(
+            scale_by(np.clip(normalize(arr)-0.1, 0, 1)**0.4, 2)-0.1,
+            0, 1)
+    """
+    IMG_DIM = 50
+    resized = resize(arr, (IMG_DIM, IMG_DIM, IMG_DIM), mode='constant')
+    plot_cube(
+            np.rollaxis(resized[:, :, :], 2, 0),
+            name=target_folder+'/voxel/'+dir_dep[label_start:label_end]+'.png')
+
+
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Parser added')
     parser.add_argument(
@@ -89,19 +155,19 @@ if __name__=="__main__":
     parser.add_argument(
             '-v', action="store", dest="dir_vox",
             default="./SUNCGtrain_3001_5000", help='npy file for voxel')
+    parser.add_argument(
+            '-t', action="store", dest="target_folder",
+            default="./target_folder", help='target folder for vis')
     parser.print_help()
     results = parser.parse_args()
 
-    arr = np.load(results.dir_dep)
-    plot_image(arr)
+    dir_dep = results.dir_dep
+    dir_vox = results.dir_vox
+    target_folder = results.target_folder
+    scan = ScanFile(dir_dep)
+    subdirs = scan.scan_subdir()
+    files = scan.scan_files()
 
-    arr = np.load(results.dir_vox)
-
-    # ignore 255 and replace it with 0
-    arr[arr == 255] = 0
-
-    # show_histogram(arr)
-    # transformed = np.clip(scale_by(np.clip(normalize(arr)-0.1, 0, 1)**0.4, 2)-0.1, 0, 1)
-    IMG_DIM = 50
-    resized = resize(arr, (IMG_DIM, IMG_DIM, IMG_DIM), mode='constant')
-    plot_cube(resized[:, ::-1, :])
+    for file_dep in files:
+        file_vox = file_dep.replace(dir_dep, dir_vox, 1)
+        plot_depvox(file_dep, file_vox, target_folder)
